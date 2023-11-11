2508976a934a384ffbd14624494c5568e9ce7174
adrian rodriguez <adferove@hotmail.com>
2023-11-11 23:06:11 +1100

    Added fetchCall method

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import  { useEffect, useState } from 'react';
+import "./App.css"; 
+import fetchCall from "./actions/fetch_call.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+      fetchCall(fetch,'https://jsonplaceholder.typicode.com/users','get', null)
+        .then((result:any) => {
+          setContacts(result);
+        });
+  
+      return () => {
+      };
+    }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <thead>
+          <tr>
+            <th>Key</th>
+            <th>Value</th>
+            <th>Reveal</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        {contacts.map((contact: any) => (
+          <tr key={contact.id}>
+            <td>{contact.id}</td>
+            <td>{contact.name}</td>
+            <td>{contact.username}</td>
+            <td>{contact.email}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+
+      {/* <div className="spinner">
+  <div className="double-bounce1"></div>
+  <div className="double-bounce2"></div>
+</div> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
